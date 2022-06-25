71721807c483b53a66e94a835858b6d60d6bf1d1
Levon <milevon@gmail.com>
2022-06-25 23:37:16 +0400

    JSHINT deleted

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from "./Header.module.css";
+
+const Header = function () {
+    return (
+        <div id="header-container">
+            <div className={s.blackBg}>
+
+            </div>
+
+        </div>
+
+    );
+};
+
+
+export default Header;
